7b0a2491d63468ce1cf3ab9a9bafa205b6d5a47a
Rubina1978 <hrubesova@gmail.com>
2025-10-16 20:18:03 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function runGame() {
+
+}
+function checkAnswer() {
+}
+function incrementScore() {
+
+}
+function iincrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+}
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+}
